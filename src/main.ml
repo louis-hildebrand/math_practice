a44dc9d8d1fbea1585ac6ade00477963c6690339
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-12-17 15:34:33 -0500

    Implement basic command-line argument parsing

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,34 @@
 open Printf
 
+(* Command-line argument parsing ------------------------------------------------------------------------------------ *)
+let usage_msg = "math_practice [-s | --silent] <subcommand> <options>"
+let silent = ref false
+let speclist = ref [
+  ("-s", Arg.Set silent, "Do not print seed");
+  ("--silent", Arg.Set silent, "Do not print seed");
+]
+let anon_args = ref []
+let subcommand = ref None
+let set_subcommand (arg: string): unit =
+  match arg with
+  | "order" -> subcommand := Some arg
+  | _ -> raise (Arg.Bad (sprintf "Invalid subcommand %s" arg))
+
+let anon_fun arg =
+  match !subcommand with
+  | None -> set_subcommand arg
+  | Some _ -> anon_args := arg :: !anon_args
+
+(* Subcommands ------------------------------------------------------------------------------------------------------ *)
+let order_of_operations (): unit =
+  (printf "Subcommand: order; silent: %B\n" !silent)
+
+let invoke_subcommand (): unit =
+  match !subcommand with
+  | Some "order" -> order_of_operations ()
+  | Some s -> raise (Arg.Bad (sprintf "Invalid subcommand %s" s))
+  | None -> raise (Arg.Bad (sprintf "No subcommand provided"))
+
 let () =
-  Math_expr.init ();
-  let e = Math_expr.next_rand 0 1 3 (-19) 20 in
-  printf "Random expression: %s\n" (Math_expr.string_of_expr e)
+  Arg.parse_dynamic speclist anon_fun usage_msg;
+  invoke_subcommand ()
