994d042c0628805eee880aa5df779a28c4599608
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-12-19 02:36:23 -0500

    Prevent division by zero in next_rand

--- lib/math_expr.ml
+++ lib/math_expr.ml
@@ -28,52 +28,16 @@ let repeat (x: 'a) (n: int): 'a list =
   in
   repeat' n []
 
+let get_rand_from_list (options: 'a list): 'a =
+  let i = Random.int (List.length options) in
+  List.nth options i
+
 (* Public functions --------------------------------------------------------- *)
 let init: unit -> unit =
   Random.self_init
 
 let seed (n: int): unit =
   Random.init n
-
-let rec next_rand' (min_depth: int) (max_depth: int) (width: int) (min_const: int) (max_const: int): expr =
-  if max_depth = 0 then 
-    Z (min_const + Random.int (max_const - min_const))
-  else
-    (* Choose type of expression *)
-    let r = if min_depth = 0 then Random.int 5 else 1 + Random.int 4 in
-    if r = 0 then
-      Z (min_const + Random.int (max_const - min_const))
-    else if r = 1 then
-      Add (next_args min_depth max_depth width min_const max_const)
-    else if r = 2 then
-      Sub (next_args min_depth max_depth width min_const max_const)
-    else if r = 3 then
-      Mul (next_args min_depth max_depth width min_const max_const)
-    else
-      let min_depth' = max 0 (min_depth - 1) in
-      let e1 = (next_rand' min_depth' (max_depth - 1) width min_const max_const) in
-      let e2 = (next_rand' min_depth' (max_depth - 1) width min_const max_const) in
-      Div (e1, e2)
-and next_args (min_depth: int) (max_depth: int) (width: int) (min_const: int) (max_const: int): expr list =
-  (* Choose width (number of arguments) *)
-  let w = 2 + Random.int (width - 1) in
-  let min_depth' = max 0 (min_depth - 1) in
-  let f = fun () -> next_rand' min_depth' (max_depth - 1) width min_const max_const in
-  List.map f (repeat () w)
-
-let next_rand (min_depth: int) (max_depth: int) (width: int) (min_const: int) (max_const: int): expr =
-  if min_depth < 0 then
-    raise (Invalid_argument "Minimum depth of expression cannot be negative.")
-  else if max_depth < 0 then
-    raise (Invalid_argument "Maximum depth of expression cannot be negative.")
-  else if min_depth > max_depth then
-    raise (Invalid_argument "Minimum depth of expression must be less than or equal to maximum depth.")
-  else if width < 2 then
-    raise (Invalid_argument "Width of expression must be at least 2.")
-  else if min_const >= max_const then
-    raise (Invalid_argument "Minimum constant must be less than maximum constant.")
-  else
-    next_rand' min_depth max_depth width min_const max_const
 
 let string_of_expr (e: expr): string =
   let rec string_of_expr' (ctxt: expr_context option) (e: expr): string =
@@ -146,3 +110,55 @@ let rec eval (e: expr): float =
       if denom = 0.0 then raise (Undefined (sprintf "Attempt to divide by zero in expression %s." (string_of_expr e)))
       else let numer = eval e1 in
       numer /. denom
+
+let choose_next_element (min_depth: int) (min_const: int) (max_const: int): int =
+  let allowed = [1; 2; 3] in
+  (* Only allow constants if the minimum depth has been reached *)
+  let allowed = if min_depth = 0 then 0::allowed else allowed in
+  (* Disallow division if 0 is the only valid constant *)
+  let allowed = if min_const != 0 || max_const != 1 then 4::allowed else allowed in
+  get_rand_from_list allowed
+
+let rec next_rand' (min_depth: int) (max_depth: int) (width: int) (min_const: int) (max_const: int): expr =
+  if max_depth = 0 then 
+    Z (min_const + Random.int (max_const - min_const))
+  else
+    (* Choose type of expression *)
+    let r = choose_next_element min_depth min_const max_const in
+    if r = 0 then
+      Z (min_const + Random.int (max_const - min_const))
+    else if r = 1 then
+      Add (next_args min_depth max_depth width min_const max_const)
+    else if r = 2 then
+      Sub (next_args min_depth max_depth width min_const max_const)
+    else if r = 3 then
+      Mul (next_args min_depth max_depth width min_const max_const)
+    else
+      let min_depth' = max 0 (min_depth - 1) in
+      let e1 = next_rand' min_depth' (max_depth - 1) width min_const max_const in
+      let e2 = get_nonzero min_depth' (max_depth - 1) width min_const max_const in
+      Div (e1, e2)
+and next_args (min_depth: int) (max_depth: int) (width: int) (min_const: int) (max_const: int): expr list =
+  (* Choose width (number of arguments) *)
+  let w = 2 + Random.int (width - 1) in
+  let min_depth' = max 0 (min_depth - 1) in
+  let f = fun () -> next_rand' min_depth' (max_depth - 1) width min_const max_const in
+  List.map f (repeat () w)
+and get_nonzero (min_depth: int) (max_depth: int) (width: int) (min_const: int) (max_const: int): expr =
+  let e = next_rand' min_depth max_depth width min_const max_const in
+  if eval e = 0.0 then get_nonzero min_depth max_depth width min_const max_const
+  else e
+
+let next_rand (min_depth: int) (max_depth: int) (width: int) (min_const: int) (max_const: int): expr =
+  if min_depth < 0 then
+    raise (Invalid_argument "Minimum depth of expression cannot be negative.")
+  else if max_depth < 0 then
+    raise (Invalid_argument "Maximum depth of expression cannot be negative.")
+  else if min_depth > max_depth then
+    raise (Invalid_argument "Minimum depth of expression must be less than or equal to maximum depth.")
+  else if width < 2 then
+    raise (Invalid_argument "Width of expression must be at least 2.")
+  else if min_const >= max_const then
+    raise (Invalid_argument "Minimum constant must be less than maximum constant.")
+  else
+    next_rand' min_depth max_depth width min_const max_const
